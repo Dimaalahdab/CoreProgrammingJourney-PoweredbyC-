b4e3c30623d1ae6d1cec81d9aca482c7c754dba5
Dimaalahdab <dimaalahdab1@gmail.com>
2025-08-02 14:21:41 +0300

    cin with basic problem

---
+++ cin_getInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main() {
+//get input
+string name , surname ;
+
+cout << "Enter your name: \n" ;
+cin >> name ;
+cout << "Enter your surname: \n" ;
+cin >> surname ;
+cout << "Your full name : \n" ;
+cout << name <<" "<< surname ;
+return 0 ;
+}
